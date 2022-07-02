1e40888762352e4ab5157750fec114fb0dc15000
nkalinich <nataliekalinich@gmail.com>
2022-07-02 19:42:16 +0300

    cards

--- script.js
+++ script.js
@@ -2,10 +2,9 @@
 // карт номиналом 2-10 всех мастей
 // карт jack, queen, king всех мастей
 // карт тузов всех мастей
-// debugger;
 
 suit =["clubs","spades","diamonds","hearts"];
-picture = ["jack","queen","king","t"];
+person = ["jack","queen","king","t"];
 cards = [];
 
 for(i = 2; i <= 10; i++) {
@@ -22,18 +21,18 @@ for(i = 2; i <= 10; i++) {
     }
 }
 
-for(i = 0; i < picture.length; i++) {
+for(i = 0; i < person.length; i++) {
     for(j = 0; j < suit.length; j++) {
         cardInfo = (`<div class="card__info">
-            ${picture[i][0].toUpperCase()}
+            ${person[i][0].toUpperCase()}
             <img src="images/${suit[j]}.svg" alt="${suit[j]}">
         </div>`);
 
-        figure = picture[i] !== "t" ? picture[i] : suit[j];
+        picture = person[i] !== "t" ? person[i] : suit[j];
 
         cards.push(`<div class="card card--person">
             ${cardInfo}
-            <img class="person" src="images/${figure}.svg" alt="${figure}">
+            <img class="person" src="images/${picture}.svg" alt="${picture}">
             ${cardInfo}
         </div>`);
     }
